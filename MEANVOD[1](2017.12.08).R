926e56a9e6437eca3aac0b77b6b5205ac35cd5d4
Patrick Nicolle <PatNicolle@Patricks-MacBook-Pro.local>
2017-12-08 16:26:02 +1100

    VOD update

--- MEANVOD[1](2017.12.08).R
+++ MEANVOD[1](2017.12.08).R
@@ -15,21 +15,32 @@ data2 <- brick(file2)
 annual_VOD <- brick()
 yearsV <- nlayers(data2)/12
 for (k in 1: yearsV) {
-  annualV <- mean(data2[[(k*12-13):(k*12)]])
+  annualV <- mean(data2[[(k*12-11):(k*12)]])
   annual_VOD <- addLayer(annual_VOD, annualV)
 }
 
-
+yearsV
 meanannual_VOD <- mean(annual_VOD) 
 
 anom.annaul.VOD<- annual_VOD- meanannual_VOD
 
 
-anombreaksV <- c(-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25)
+anombreaksV <- c(-0.3, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.3)
 
-anomcolsV <- colorRampPalette(c( "red","sienna2", "sienna4", "wheat3", "lightgrey", "darkolivegreen4","darkolivegreen3","darkolivegreen2" )) 
+anomcolsV <- colorRampPalette(c("red", "red", "orangered4", "rosybrown2","lightgrey", "lightgrey", "darkseagreen", "olivedrab2", "limegreen", "limegreen")) 
 
-plot(anom.annaul.VOD[[1:5]], breaks=anombreaksV, col=anomcolsV(length(breaks)-1)main= c("1993" : "1997")) 
+pdf(file = "VODIMAGES3.pdf", height = 12, width = 14)
+par(mfrow=c(4,5))
+par(mai=c(0.2,0.5,0.2,0.5))
+par(omi=c(0.2,0.2,0.2,0.2))
+for( k in 1:20) {
+  
+  plot(anom.annaul.VOD[[k]], breaks=anombreaksV, col=anomcolsV(length(breaks)-1), main=(k+1992))
+ 
+}
+dev.off()
+
+
 
 # 1-1982  
 
