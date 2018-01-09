93687fb8a015b772d3aba80c35d2ccbedd51be11
Patrick Nicolle <PatNicolle@Patricks-MacBook-Pro.local>
2018-01-09 16:50:10 +1100

    addition another one

--- NDVIvsRainfallloops.R
+++ NDVIvsRainfallloops.R
@@ -8,7 +8,7 @@ library(raster)
 library(SPAr) 
 library(ncdf4) 
 library(RNetCDF) 
-
+library(segmented)
 
 file.ndvi <- list.files("/Volumes/P_Harddrive/Annual_data/ndvi_1982_2011_Australia/",full.names = TRUE)
 file.precip <- list.files("/Volumes/P_Harddrive/Annual_data/Precipitation/", full.names = TRUE)
@@ -79,8 +79,30 @@ rainfalldry[rainfalldry>meanprecip]<- NA
 rainfallwet[rainfallwet<meanprecip]<- NA
 
 #generating a value for slope for ndvi vs rainfall in high rain and low rain years
-
-plot(x= values(rainfall), y= values(ndvi)) 
-lm1.lm <- lm(values(ndvi)~values(rainfall)) 
+x <- as.vector(values(rainfall))
+y <- as.vector(values(ndvi))
+x2 <- as.vector(values(rainfalldry))
+x3 <- as.vector(values(rainfallwet))
+#regular 
+pdf("regualrndvirainfall.pdf")
+plot(x,y) 
+lm1.lm <- lm(y~x) 
 abline(lm1.lm, col=3)
-
+dev.off()
+#dry
+pdf("dryndvirainfall.pdf")
+plot(x2,y) 
+lm2.lm <- lm(y~x2) 
+abline(lm2.lm, col=2) 
+dev.off()
+#wet 
+pdf("wetndvirainfall.pdf")
+plot(x3,y) 
+lm3.lm <- lm(y~x3) 
+abline(lm3.lm, col=4)
+dev.off()
+plot(1, type="n", xlab="", ylab="", xlim=c(0, 5000), ylim=c(0, 1))
+abline(lm1.lm, col=1)
+abline(lm2.lm, col=2)
+abline(lm3.lm, col=4)
+segmented_lm <- segmented(lm1.lm, seg.Z = ~x, psi = 300)
